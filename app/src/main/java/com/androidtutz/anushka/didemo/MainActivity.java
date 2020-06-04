b1f7741e6933d6c24a2cc7341d5a8be1f9f23dc7
SinaSystem <sinasystem47@gmail.com>
2020-06-05 00:41:34 +0430

    Initial commit

---
+++ app/src/main/java/com/androidtutz/anushka/didemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.androidtutz.anushka.didemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+    @Inject
+    SmartPhone smartPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        App.getApp().getSmartPhoneComponent().inject(this);
+
+        smartPhone.makeACall();
+    }
+}
